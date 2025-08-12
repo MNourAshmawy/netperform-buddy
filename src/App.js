582823d1eda7181dbf5b20b7b22ce005942182b8
Mohamed Nour Ashmawy <mohamed.n.ashmawy@gmail.com>
2025-08-12 20:05:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function App() {
+  return (
+    <div className="App">
+      <h1>NetPerform Buddy</h1>
+      <p>Your app is running!</p>
+    </div>
+  );
+}
+
+export default App;
